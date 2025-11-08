661c13ac9e3114df735e61ab9213fe7c93013d40
13432832724 <l2979007648@163.com>
2025-11-08 12:09:05 +0800

    v14.0.3 新增 设置app 适配: USB提示-取消

---
+++ src/apps/com.android.settings.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.android.settings',
+  name: '设置',
+  groups: [
+    {
+      key: 1,
+      name: '插数据线连接电脑-USB提示-取消',
+      desc: 'USB用于xxx -取消',
+      rules: [
+        {
+          matches: [
+            '[vid="alertTitle"][text="USB 用于"]',
+            '[text="取消"][visibleToUser=true]',
+          ],
+          fastQuery: true,
+          snapshotUrls: 'https://i.gkd.li/i/23428639',
+          activityIds: '.connecteddevice.usb.UsbModeChooserActivity',
+        },
+      ],
+    },
+  ],
+});
