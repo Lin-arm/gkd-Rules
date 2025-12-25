c81ab3985b657d2e4f50764fac0e607302e4253f
13432832724 <l2979007648@163.com>
2025-12-25 18:44:16 +0800

    v31.0.3  tgPlus 新增 更多-下载①

--- src/apps/org.telegram.plus.ts
+++ src/apps/org.telegram.plus.ts
@@ -6,17 +6,30 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '更多-下载-单个图片or视频',
+      name: '更多-下载①',
+      desc: '右上角菜单-点击 下载',
+      enable: false,
+      rules: [
+        {
+          matches: '@[clickable=true] > [text="保存到相册"]',
+          fastQuery: true,
+          snapshotUrls: 'https://i.gkd.li/i/24337783',
+          activityIds: 'org.telegram.ui.LaunchActivity',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '更多-下载②-单个媒体',
       desc: '弹窗-点击 这张图片or这个媒体',
       rules: [
         {
-          matches: [
-            '[text="保存媒体文件"] < * +2 * > TextView[text="这张图片" || text="这个媒体"][visibleToUser=true]',
-          ],
+          matches:
+            '[text="取消"] + TextView[text="这张图片" || text="这个媒体"][clickable=true]',
           fastQuery: true,
           snapshotUrls: [
-            'https://i.gkd.li/i/22945715',
-            'https://i.gkd.li/i/22945853',
+            'https://i.gkd.li/i/22945715', //图片
+            'https://i.gkd.li/i/22945853', //视频
           ],
           activityIds: 'org.telegram.ui.LaunchActivity',
         },
