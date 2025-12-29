fd7a7c78f9a6593fbb11add2ead87b4bbfd6f4e2
13432832724 <l2979007648@163.com>
2025-12-29 21:53:36 +0800

    v32.0.3 自动精灵 更新提示-取消

--- src/apps/com.zdanjian.zdanjian.ts
+++ src/apps/com.zdanjian.zdanjian.ts
@@ -6,6 +6,20 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
+      name: '更新提示-取消',
+      desc: '点击 取消',
+      rules: [
+        {
+          matches:
+            '[text="发现新版本"] < * +(3,4) * > [text="取消"][clickable=true]',
+          fastQuery: true,
+          snapshotUrls: 'https://i.gkd.li/i/24369119',
+          activityIds: '.activities.home.HomeActivity',
+        },
+      ],
+    },
+    {
+      key: 2,
       name: '运行脚本-弹窗-确定',
       desc: '①分辨率不匹配 ②系统无障碍',
       rules: [
